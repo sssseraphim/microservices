252cab4bd2bb1c63ff0acf98228862e09b81e142
sssseraphim <tarasovv2004@ya.ru>
2026-02-12 23:31:52 +0300

    added ports interfaces

--- order/internal/application/core/api/api.go
+++ order/internal/application/core/api/api.go
@@ -9,4 +9,15 @@ type Application struct {
 	db ports.DBPort
 }
 
-func NewApplication(db.
+func NewApplication(db ports.DBPort) *Application {
+	return &Application{
+		db: db}
+}
+
+func (a Application) PlaceOrder(order domain.Order) (domain.Order, error) {
+	err := a.db.Save(&order)
+	if err != nil {
+		return domain.Order{}, err
+	}
+	return order, nil
+}
